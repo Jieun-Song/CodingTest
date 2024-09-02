d96c6942eabcae78663303f3ccbcab62d64c2a13
jieun song <0scarl1216@mju.ac.kr>
2024-09-02 23:28:19 +0900

    [20240902]백준풀이 1343

--- silver/1343.py
+++ silver/1343.py
@@ -1,28 +1,34 @@
 # AAAA BB
 
 def listSplit(list):
+  #문자열끼리 더하고 append하고, *끼리 더하고 append하고
+
   result = []
+  appendString = ""
+  
   for i in list :
-    if i != "":
-      result.append(i)
+    if i == "*":
+      stars = stars + i
+    else :
+
+  result.append(i)
+  print(result)
   return result
 
 try:
   # .으로 나눈 문자열들이 짝수 문자열이어야함
   # .으로 나눈 문자열을 4로 먼저 나누고 나머지가 2이면 가능.
-  list = listSplit(input().split("."))
+  list = listSplit(input().split())
 
   for i in list:
     fourTime = len(i) // 4
     left = len(i) % 4
     if left % 2 == 1:
-      raise EOFError
-    print
+      raise ValueError
+    
 except:
   print(-1)
 
 
 # 해야할 것
-# .의 갯수들 구하기(결과 출력용)
-# 적절한 error를 raise하기
 # 출력하기
