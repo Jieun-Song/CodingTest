866395cacbdeeb02cd110fb4f149a7c48df672e3
jieun song <0scarl1216@mju.ac.kr>
2025-01-13 19:32:02 +0900

    [20250113]1343백준풀이

--- silver/1343.py
+++ silver/1343.py
@@ -1,34 +1,17 @@
-# AAAA BB
+got = input()
+list = got.split(".")
+# print(list)
+result = ""
+for i in list:
+    if i != "":
+        n = len(i)
+        iter = n // 4
+        left = n % 4
+        if left % 2 != 0:
+            result = "-1 "
+            break
+        result = result + "AAAA"*iter+"BB"*(left//2)+ "."
+    else:
+        result = result + "."
 
-def listSplit(list):
-  #문자열끼리 더하고 append하고, *끼리 더하고 append하고
-
-  result = []
-  appendString = ""
-  
-  for i in list :
-    if i == "*":
-      stars = stars + i
-    else :
-
-  result.append(i)
-  print(result)
-  return result
-
-try:
-  # .으로 나눈 문자열들이 짝수 문자열이어야함
-  # .으로 나눈 문자열을 4로 먼저 나누고 나머지가 2이면 가능.
-  list = listSplit(input().split())
-
-  for i in list:
-    fourTime = len(i) // 4
-    left = len(i) % 4
-    if left % 2 == 1:
-      raise ValueError
-    
-except:
-  print(-1)
-
-
-# 해야할 것
-# 출력하기
+print(result[:-1])
